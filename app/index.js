2f633c49403c81373a29e387c5ee68e58f521e14
Slava Isaev <slava.isaev@gmail.com>
2021-03-31 15:59:38 +0000

    Adding Redis.

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const redis   = require("redis");
+
+const PORT=8080;
+const VISITS="visits";
+
+const app = express();
+
+
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+client.set(VISITS, 0);
+
+app.get("/", (req, res)=>{
+    console.log(req.method+" from IP:"+req.ip);
+    client.get(VISITS, (err, visits)=>{
+        res.send("Total number of visits from start:"+visits);
+        client.incr(VISITS);
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port: "+PORT);
+});
